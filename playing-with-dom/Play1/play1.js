e316b4b2ce87d123da58a3be02c10a9676202bdf
RafexStrike <adnanrahmanrafi515@gmail.com>
2025-01-16 16:52:50 +0600

    gonna add a button now with js

---
+++ playing-with-dom/Play1/play1.js
@@ -0,0 +1,12 @@
+const box1 = document.getElementById('box-1')
+box1.style.backgroundColor = "red";
+box1.style.color = "white";
+box1.style.fontWeight = "bold";
+
+const box2 = document.getElementById("box-2")
+box2.innerText = "hi how are you";
+
+const box5 = document.getElementsByTagName("div");
+// console.log(box5)
+
+

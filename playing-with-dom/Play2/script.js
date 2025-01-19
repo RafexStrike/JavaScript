b84479a7ace68e74c88162082bdfd01dcccf1833
RafexStrike <adnanrahmanrafi515@gmail.com>
2025-01-19 14:50:19 +0600

    chatgpt created a dummy website to play with DOM. and it's great!

---
+++ playing-with-dom/Play2/script.js
@@ -0,0 +1,8 @@
+const headingID = document.getElementById("main-title")
+const headingText = headingID.innerText
+
+console.log(headingText)
+
+
+const nameFieldID = document.getElementById("name")
+nameFieldID.value = headingText
